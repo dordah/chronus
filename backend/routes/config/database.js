bcfcb067cec93b90eb10c4805c03af8267a132ab
dordahan <dor@taranis.ag>
2020-12-16 22:53:06 +0200

    JWT login

--- backend/routes/config/database.js
+++ backend/routes/config/database.js
@@ -1,14 +1,14 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize('v1', 'postgres', 'united', {
-  host: 'localhost',
-  dialect: 'postgres',
+module.exports = new Sequelize("v1", "postgres", "united", {
+  host: "localhost",
+  dialect: "postgres",
   operatorsAliases: false,
 
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 });
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const { Pool, Client } = require("pg");
@@ -7,11 +8,12 @@ const exphbs = require("express-handlebars");
 const conncetionString = "postgressql://postgres:united@localhost:5432/v1";
 const path = require("path");
 const exp = require("express-handlebars");
-
 const app = express();
+const jwt = require("jsonwebtoken");
 
 // Database
 const db = require("./config/database");
+const { nextTick } = require("process");
 
 // Test db connection
 db.authenticate()
@@ -40,10 +42,6 @@ app.get("/apiv1/home", (req, res) => {
   res.sendStatus(200);
 });
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
-});
-
 const posts = [
   {
     username: "Boaz",
@@ -54,6 +52,30 @@ const posts = [
     title: "Post 2",
   },
 ];
+
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
+});
+
+app.post("/login", (req, res) => {
+  // Authenticat User
+  const username = req.body.username;
+  const user = { name: username };
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  res.json({ accessToken: accessToken });
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.post("/apiv1/user/register", (req, res) => {
   console.log(req.body);
