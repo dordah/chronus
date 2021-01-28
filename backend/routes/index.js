4047f937842c27ea20d281bf62e21208f9dddeb7
boazt <boaztouval11@gmail.com>
2021-01-28 10:11:29 +0200

    fixing issues with postman and Docker

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,7 +5,7 @@ const { Pool, Client } = require("pg");
 const { query } = require("express");
 const router = express.Router();
 const exphbs = require("express-handlebars");
-const conncetionString = "postgressql://postgres:united@localhost:5432/v1";
+// const conncetionString = "postgressql://postgres:united@localhost:5432/v1";
 const path = require("path");
 const exp = require("express-handlebars");
 const app = express();
@@ -13,29 +13,29 @@ const jwt = require("jsonwebtoken");
 var cookieParser = require("cookie-parser");
 
 // Database
-const db = require("./config/database");
+// const db = require("./config/database");
 const { nextTick } = require("process");
 
 // Test db connection
-db.authenticate()
-  .then(() => console.log("Database connected..."))
-  .catch((err) => console.log("Error: " + err));
+// db.authenticate()
+//   .then(() => console.log("Database connected..."))
+//   .catch((err) => console.log("Error: " + err));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-const client = new Client({
-  connectionString: conncetionString,
-});
+// const client = new Client({
+//   connectionString: conncetionString,
+// });
 
-client.connect();
+// client.connect();
 
 // client.query(`SELECT * from users where name = '${name}'`,(err, res)=>{
 //   console.log(err,res)
 // })
 
 /* GET home page. */
-app.get("/apiv1/home", (req, res) => {
+app.get("/apiv1/home%0A", (req, res) => {
   res.render("index", { title: "Express" });
   res.sendStatus(200);
 });
@@ -51,7 +51,7 @@ const posts = [
   },
 ];
 
-app.get("/posts", authenticateToken, (req, res) => {
+app.get("/posts%0A", authenticateToken, (req, res) => {
   username = posts.filter((post) => post.username === req.user.name);
   res.json({ username });
 });
@@ -59,6 +59,7 @@ app.get("/posts", authenticateToken, (req, res) => {
 app.post("/login", (req, res) => {
   // Authenticat User
   const username = req.body.username;
+  console.log(username);
   const password = req.body.password;
   // Send back to client json with all data (Front) - (DB) - Check if user exist && Password ? Return user's details : Deny (401)
   // TODO - Set cookie for client -> { accessToken(Data) }
@@ -94,14 +95,14 @@ function authenticateToken(req, res, next) {
   });
 }
 
-app.post("/apiv1/user/register", (req, res) => {
+app.post("/apiv1/user/register%0A", (req, res) => {
   console.log(req.body);
   res.render("index", { title: "Hello, GET request at apiv1/user/register" });
   res.render("index", { title: req.body });
 });
 
 // User routes
-app.use("/apiv1/user", require("./user"));
+app.use("/apiv1/user%0A", require("./user"));
 
 // Main route
 app.use("/apiv1", router);
